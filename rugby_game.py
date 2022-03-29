012d0d1acc86e81dc700420caf3d84f8b88d1819
ffaure32 <frfaure32@gmail.com>
2022-03-29 16:15:21 +0200

    drop on both sides

--- rugby_game.py
+++ rugby_game.py
@@ -1,3 +1,17 @@
 class RugbyGame:
-    pass
+
+    def __init__(self, equipe_1, equipe_2):
+        self.equipe_1 = equipe_1
+        self.equipe_2 = equipe_2
+        self.score_1 = 0
+        self.score_2 = 0
+
+    def get_score(self):
+        return "{}-{}".format(self.score_1, self.score_2)
+
+    def drop(self, equipe):
+        if equipe == self.equipe_1:
+            self.score_1 += 3
+        else:
+            self.score_2 += 3
 
--- test_rugby_game.py
+++ test_rugby_game.py
@@ -4,5 +4,26 @@ from rugby_game import RugbyGame
 
 
 class RugbyGameTest(TestCase):
+
+    def setUp(self):
+        self.game = RugbyGame("UBB", "Toulouse")
+
     def test_init_game(self):
-        self.fail()
+
+        assert self.game.get_score() == "0-0"
+
+    def test_drop_de_jalibert(self):
+        self.game.drop("UBB")
+
+        assert self.game.get_score() == "3-0"
+
+    def test_drop_de_ntamack(self):
+        self.game.drop("Toulouse")
+
+        assert self.game.get_score() == "0-3"
+
+    def test_drop_des_grandisse(self):
+        self.game.drop("Toulouse")
+        self.game.drop("UBB")
+
+        assert self.game.get_score() == "3-3"
